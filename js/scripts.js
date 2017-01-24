b8b2a80ad78a1933596c040f7dc4d071ff7bd319
Kaz Matthews and Tammy Dang <kaz@>
2017-01-24 14:24:46 -0800

    add object constructor for recipt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,72 @@
 // //Business Logic
-// function Movies(user, title, time, age, price) {
-//   this.user = user;
-//   this.title = title;
-//   this.time = time;
-//   this.age = age;
-//   this.price = price;
-// }
-//
-// Movies.prototype.displayInfo = function() {
-//   return "Thanks, " + this.user " you have selected" + this.title + " for" + this.time + " and the total price will be" + this.price + ".";
-// }
+function MoviesValue(name, title, time, age) {
+  this.name = name;
+  this.title = title;
+  this.time = time;
+  this.age = age;
+}
+
+function Movies(userName, titleMovie, showTime, inputAge) {
+  this.userName = userName;
+  this.titleMovie = titleMovie;
+  this.showTime = showTime;
+  this.inputAge = inputAge;
+}
+Movies.prototype.receipt = function() {
+  return "Thanks, " + this.userName + "! You have selected" + this.titleMovie + " for " + this.showTime + " and the total price will be: ";
+}
+MoviesValue.prototype.price = function() {
+  var moviePrice = 15;
+  if(this.age <=10 || this.age >=65) {
+    moviePrice -= 5;
+  }
+  if(this.title === 1) {
+    moviePrice -=2;
+  }
+  if(this.time === 1) {
+    moviePrice -=3;
+    }
+  return "$" + moviePrice;
+}
 
 //User Interface
 $(document).ready(function () {
 
   var movie = 0;
+  var titleOfMovie = "";
   $("#lalalandTitle").click(function() {
     $("#lalalandSelect").show();
     movie = 2;
+    titleOfMovie = "La La Land";
   });
   $("#xxxTitle").click(function() {
     $("#xxxSelect").show();
     movie = 2;
+    titleOfMovie = "XXX";
   });
   $("#dogsTitle").click(function() {
     $("#dogsSelect").show();
     movie = 2;
+    titleOfMovie = "A Dog's Purpose";
   });
   $("#findDoryTitle").click(function() {
     $("#findDorySelect").show();
     movie = 1;
+    titleOfMovie = "Finding Dory";
   });
 
   $("#movie").submit(function(event) {
     event.preventDefault();
 
-    var age = parseInt($("input#ageInput").val());
-    var time = parseInt($("#lalalandOpt option:selected").val()) + parseInt($("#xxxOpt option:selected").val()) + parseInt($("#dogsOpt option:selected").val()) + parseInt($("#findOpt option:selected").val());
-    console.log(movie);
+    var ageInput = parseInt($("input#ageInput").val());
+    var timeInput = parseInt($("#lalalandOpt option:selected").val()) + parseInt($("#xxxOpt option:selected").val()) + parseInt($("#dogsOpt option:selected").val()) + parseInt($("#findOpt option:selected").val());
+    var movieTitle = movie;
+    var nameInput = $("#nameInput").val();
+    var titleOfMovieFinal = titleOfMovie;
 
+    var ticket = new MoviesValue(nameInput, movieTitle, timeInput, ageInput);
+    console.log(ticket);
+    console.log(ticket.price());
+    $(".results").append(ticket.receipt() + )
   })
 });
