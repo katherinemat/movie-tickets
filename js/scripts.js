58acaaa856b96d997605170d022b2b534f11a24e
Kaz Matthews and Tammy Dang <tammy@>
2017-01-24 11:10:58 -0800

    add css and js so that when user clicks on movie title, form with times appears.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-//Business Logic
-function Movies(user, title, time, age, price) {
-  this.user = user;
-  this.title = title;
-  this.time = time;
-  this.age = age;
-  this.price = price;
-}
-
-Movies.prototype.displayInfo = function() {
-  return "Thanks, " + this.user " you have selected" + this.title + " for" + this.time + " and the total price will be" + this.price + ".";
-}
+// //Business Logic
+// function Movies(user, title, time, age, price) {
+//   this.user = user;
+//   this.title = title;
+//   this.time = time;
+//   this.age = age;
+//   this.price = price;
+// }
+//
+// Movies.prototype.displayInfo = function() {
+//   return "Thanks, " + this.user " you have selected" + this.title + " for" + this.time + " and the total price will be" + this.price + ".";
+// }
 
 //User Interface
+$(document).ready(function () {
+  $("#lalalandTitle").click(function() {
+    $("#lalalandSelect").show();
+  });
+  $("#xxxTitle").click(function() {
+    $("#xxxSelect").show();
+  });
+  $("#dogsTitle").click(function() {
+    $("#dogsSelect").show();
+  });
+  $("#findDoryTitle").click(function() {
+    $("#findDorySelect").show();
+  });
+});
